0969f7069c8da7d6fa76443bd253fd999ac9ef98
przeor <kamil@przeorski.pl>
2016-03-10 12:47:51 +0100

    CO second route from mongodb + toObject() mongoose hotfix

--- server/routes.js
+++ server/routes.js
@@ -30,33 +30,16 @@ let PublishingAppRoutes = [{
     return Article.find({}, function(err, articlesDocs) {
       return articlesDocs;
     }).then ((articlesArrayFromDB) => {
-
-      console.info("articlesArrayFromDB", articlesArrayFromDB);
-
-      articlesArrayFromDB = [{
-        "articleId": "987654",
-        "articleTitle": "BACKEND Lorem ipsum - article one",
-        "articleContent": "BACKEND Here goes the content of the article"
-      }, {
-        "articleId": "123456",
-        "articleTitle": "BACKEND Lorem ipsum - article two",
-        "articleContent": "BACKEND Sky is the limit, the content goes here."
-      }]; // That are our mocked articles from MongoDB
-
-
-
       let results = [];
       articlesIndex.forEach((index) => {
-        let singleArticleObject = articlesArrayFromDB[index];
+        let singleArticleObject = articlesArrayFromDB[index].toObject();
         let falcorSingleArticleResult = {
           path: ['articles', index],
           value: singleArticleObject
         };
-        console.info("index"+index, falcorSingleArticleResult);
         results.push(falcorSingleArticleResult);
       });
       console.info(">>>> results", results);
-      console.info("********");
       return results;
     })
   }
