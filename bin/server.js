53b3c9072682c83cce4ece0309bb0588b0bea9c8
przeor <kamil@przeorski.pl>
2016-04-05 12:19:45 +0200

    heroku hotfix v1

--- bin/server.js
+++ bin/server.js
@@ -7,8 +7,8 @@ const config = require('../config');
 const host = config.get('server_host');
 const port = config.get('server_port');
 
-server.listen(port, host, function () {
+server.listen(port, function () {
   console.log(chalk.green(
-    `Server is now running at ${host}:${port}.`
+    `Server is now running at host:${port}.`
   ));
 });
