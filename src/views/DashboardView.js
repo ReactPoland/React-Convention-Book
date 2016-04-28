3f88cf17c2092e21880070bec83d997144572a03
przeor <kamil@przeorski.pl>
2016-04-28 15:21:32 +0200

    CO dashboard articles list v1

--- src/views/DashboardView.js
+++ src/views/DashboardView.js
@@ -15,15 +15,57 @@ const mapDispatchToProps = (dispatch) => ({
 
 });
 
+
+import { List, ListItem } from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+import FileFolder from 'material-ui/svg-icons/file/folder';
+
+const ListExampleFolder = () => (
+    <List>
+      <ListItem
+        leftAvatar={<Avatar icon={<ActionInfo />} />}
+        primaryText="Photos"
+        secondaryText="Jan 9, 2014"
+      />
+      <ListItem
+        leftAvatar={<Avatar icon={<ActionInfo />} />}
+        primaryText="Recipes"
+        secondaryText="Jan 17, 2014"
+      />
+      <ListItem
+        leftAvatar={<Avatar icon={<ActionInfo />} />}
+        primaryText="Work"
+        secondaryText="Jan 28, 2014"
+      />
+    </List>
+);
+
 class DashboardView extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render () {
+    
+    let articlesJSX = [];
+    for(let articleKey in this.props.article) {
+      let articleDetails = this.props.article[articleKey];
+      let currentArticleJSX = (
+        <ListItem
+          leftAvatar={<img src="/static/placeholder.png" width="50" height="50" />}
+          primaryText={articleDetails.articleTitle}
+          secondaryText={articleDetails.articleContent}
+        />
+      );
+
+      articlesJSX.push(currentArticleJSX);
+    }
     return (
-      <div>
-          <h1>Dashboard - loggedin!</h1>
+      <div style={{height: '100%', width: '75%', margin: 'auto'}}>
+        <List>
+          {articlesJSX}
+        </List>
       </div>
     );
   }
