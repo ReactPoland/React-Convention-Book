a038034773139f3fdd5848df1264610629707af1
przeor <kamil@przeorski.pl>
2016-05-01 10:46:28 +0200

    CO WYSWIG init (debug in progress

--- src/views/articles/AddArticleView.js
+++ src/views/articles/AddArticleView.js
@@ -5,7 +5,7 @@ import Falcor from 'falcor';
 import falcorModel from '../../falcorModel.js';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import WYSWIGeditor from '../../components/articles/WYSWIGeditor.js';
+import RichEditor from '../../components/wyswig-draftjs/RichEditor';
 
 const mapStateToProps = (state) => ({
 	...state
@@ -18,13 +18,29 @@ const mapDispatchToProps = (dispatch) => ({
 class AddArticleView extends React.Component {
   constructor(props) {
     super(props);
+    this._onchangeDraftJSON = this._onchangeDraftJSON.bind(this);
+
+    this.state = {
+      contentJSON: {}
+    };
   }
+
+  _onchangeDraftJSON(contentJSON, descriptionName) {
+    console.info('contentJSON', contentJSON);
+    this.setState({contentJSON: contentJSON});
+  }
+
 
   render () {
     return (
       <div style={{height: '100%', width: '75%', margin: 'auto'}}>
         <h1>Add Article</h1>
-        <WYSWIGeditor />
+          <RichEditor
+            tabIndexProp="100005"
+            initialValue={''}
+            name="description2"
+            title="Description (Level 2)"
+            onChangeTextJSON={this._onchangeDraftJSON} />
       </div>
     );
   }
