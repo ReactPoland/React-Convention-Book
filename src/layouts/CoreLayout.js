e3bc0b4483a9134b773aa033c816a1191677ebbf
przeor <kamil@przeorski.pl>
2016-05-01 11:35:27 +0200

    PoC 3

--- src/layouts/CoreLayout.js
+++ src/layouts/CoreLayout.js
@@ -12,35 +12,55 @@ import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import IconButton from 'material-ui/IconButton';
 import ActionHome from 'material-ui/svg-icons/action/home';
-import RichEditor from '../components/wyswig-draftjs/RichEditor';
-import AddArticleView from '../views/articles/AddArticleView';
+
 
 
 
 
 
 class CoreLayout extends React.Component {
+  static propTypes = {
+    children : React.PropTypes.element
+  }
+
   constructor(props) {
     super(props);
-    this._onchangeDraftJSON = this._onchangeDraftJSON.bind(this);
 
-    this.state = {
-      contentJSON: {}
-    };
   }
-
-  _onchangeDraftJSON(contentJSON, descriptionName) {
-    console.info('contentJSON', contentJSON);
-    this.setState({contentJSON: contentJSON});
-  }
-
 
   render () {
+    const buttonStyle = {
+      margin: 5
+    };
+    const homeIconStyle = {
+      margin: 5,
+      paddingTop: 5
+    };
+    
+    let menuLinksJSX;
+    let userIsLoggedIn = typeof localStorage !== 'undefined' && localStorage.token && this.props.routes[1].name !== 'logout';
+    
+    if(userIsLoggedIn) {
+      menuLinksJSX = (<span>
+          <Link to='/dashboard'><RaisedButton label="Dashboard" style={buttonStyle}  /></Link> 
+          <Link to='/logout'><RaisedButton label="Logout" style={buttonStyle}  /></Link> 
+        </span>);
+    } else {
+      menuLinksJSX = (<span>
+          <Link to='/register'><RaisedButton label="Register" style={buttonStyle}  /></Link> 
+          <Link to='/login'><RaisedButton label="Login" style={buttonStyle}  /></Link> 
+        </span>);
+    }
+
+    let homePageButtonJSX = (<Link to='/'>
+        <RaisedButton label={<ActionHome />} style={homeIconStyle}  />
+      </Link>);
+
+
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <div style={{height: '100%', width: '75%', margin: 'auto'}}>
-          <h1>Add Article222</h1>
-            <AddArticleView />
+        <div>
+            {this.props.children}
         </div>
       </MuiThemeProvider>
     );
