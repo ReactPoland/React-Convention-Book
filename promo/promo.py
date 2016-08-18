e4641774a83d215b473e1b289c4076b464caba8d
przeor <kamil@przeorski.pl>
2016-08-18 10:51:28 +0200

    WW improv + promo Improv

--- promo/promo.py
+++ promo/promo.py
@@ -4,11 +4,12 @@ ghlist = open('ghlist.txt', 'r+').read()
 print ghlist.split('\n')
 print '------'
 
-for url in ghlist.split('\n')[0:1]:
+for url in ghlist.split('\n')[0:]:
 	visitedGH = open('visitedGH.txt', 'r+').read()
 	if( url+'END' in visitedGH): 
 		print 'exists'
 		continue
+	raw_input("type smth: ")
 	print 'url'
 	print url
 	open('visitedGH.txt', 'a').write(url+'END \n')
